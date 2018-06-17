be99cf72d7b12177670358034c64abd70ee93d2e
etuna <etuna@ku.edu.tr>
2018-06-17 03:13:13 +0300

    first commit

---
+++ Seaty/SeatySecurity.cs
@@ -0,0 +1,27 @@
+﻿using SeatyDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seaty
+{
+
+
+    public class SeatySecurity
+    {
+
+
+        public static bool Login(string username, string password)
+        {
+            using (CinemaSeatingEntities entities = new CinemaSeatingEntities())
+            {
+                return entities.Users.Any(user => (user.username == username && user.password == password));
+            }
+        }
+
+    }
+
+
+
+}
